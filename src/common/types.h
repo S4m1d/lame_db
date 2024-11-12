9e9727d01fe9b034b62d1f089e7eb4d21c5b86cf
s4m1d <iams4m1d@gmail.com>
2024-11-12 23:08:03 +0500

    added writing and reading to/from files table info

--- src/common/types.h
+++ src/common/types.h
@@ -5,7 +5,7 @@ enum DataType {
   DT_TEXT,
 };
 
-#define COL_NAME_LENGTH_MAX 51
+#define COL_NAME_LENGTH_MAX 11
 
 typedef struct {
   enum DataType t;
--- src/storage/table.c
+++ src/storage/table.c
@@ -1,3 +1,77 @@
 #include "../common/types.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// int create_table(ColumnDefinition *column_defs, int columns_count) {}
+int create_table(const char *table_name, ColumnDefinition *column_defs,
+                 int columns_count) {
+
+  char name[strlen(table_name) + 6];
+  snprintf(name, sizeof(name), "%s.ldbt", table_name);
+
+  FILE *f = fopen(name, "wb");
+  if (f == NULL) {
+    printf("failed open/create file %s", name);
+    return -1;
+  }
+
+  // writing number of columns at the beginning of the file
+  int write_res = fwrite(&columns_count, sizeof(int), 1, f);
+  if (write_res < 0) {
+    printf("failed write to file %s, code %d", name, write_res);
+    exit(write_res);
+  }
+
+  for (int i = 0; i < columns_count; i++) {
+    int write_res = fwrite(&column_defs[i], sizeof(ColumnDefinition), 1, f);
+    if (write_res < 0) {
+      printf("failed write to file %s, code %d", name, write_res);
+      exit(write_res);
+    }
+  }
+
+  int close_res = fclose(f);
+  if (close_res != 0) {
+    printf("failed to close file %s, code %d", name, close_res);
+    exit(-close_res);
+  }
+
+  return 0;
+}
+
+ColumnDefinition *read_table_definition(const char *table_name,
+                                        int *columns_count) {
+  char name[strlen(table_name) + 6];
+  snprintf(name, sizeof(name), "%s.ldbt", table_name);
+
+  FILE *f = fopen(name, "rb");
+  if (f == NULL) {
+    printf("failed open/create file %s", name);
+    return NULL;
+  }
+
+  *columns_count = 0;
+  int read_res = fread(columns_count, sizeof(int), 1, f);
+  if (read_res < 0) {
+    printf("failed read columns count in file %s, code %d", name, read_res);
+    exit(read_res);
+  }
+
+  ColumnDefinition *column_defs =
+      malloc(*columns_count * sizeof(ColumnDefinition));
+
+  read_res = fread(column_defs, sizeof(int), (unsigned long)columns_count, f);
+  if (read_res < 0) {
+    printf("failed read column definitions in file %s, code %d", name,
+           read_res);
+    exit(read_res);
+  }
+
+  int close_res = fclose(f);
+  if (close_res != 0) {
+    printf("failed to close file %s, code %d", name, close_res);
+    exit(-close_res);
+  }
+
+  return 0;
+}
